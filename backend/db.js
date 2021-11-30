003a46b5e64c06a504b730efeb01fcfb1ce3df3c
Aditya-Kamarsu <aditya.k@comakeit.com>
2021-11-30 15:28:08 +0530

    temporary express

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+
+
+const connectToMongo =  async () =>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected");
+    })
+}
+
+module.exports = connectToMongo;
