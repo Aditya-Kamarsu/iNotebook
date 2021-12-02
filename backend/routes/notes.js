6b361f9043ac23936a1843ed4a3b17976bc0bd27
Aditya-Kamarsu <aditya.k@comakeit.com>
2021-12-02 12:21:26 +0530

    added update note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,80 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
+const fetchuser = require("../middleware/fetchuser");
 
-router.get('/',(req,res)=>{
-    res.json([])
-})
+//Route 1 get all notes using get "/api/notes/fetchallnotes" login req
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Some Error occurred");
+  }
+});
 
-module.exports = router
+//Route 2 add new note using post "/api/notes/addnote" login req
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const { title, description, tag } = req.body;
+    //if errors are there, return bad req
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some Error occurred");
+    }
+  }
+);
+
+//Route 3 update note using post "/api/notes/updatenote" login req
+
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  //Create a new note object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //find the note to be updated
+  let note = await Notes.findById(req.params.id);
+  if (!note) {
+    res.status(404).send("Not found");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not allowed");
+  }
+  note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+  res.json(note);
+});
+
+module.exports = router;
